6b048c97fcf42f8a37817794641dfbb1603bec7f
swan28 <swati28.g@gmail.com>
2025-02-09 22:56:14 +0530

    constants

---
+++ src/constant/__init__.py
@@ -0,0 +1,22 @@
+ARTIFACTS_DIR = "artifacts"
+LOGS_DIR = 'logs'
+LOGS_FILE_NAME = 'ner.log'
+
+
+DATA_TRANSFORMATION_ARTIFACTS_DIR = "DataTransformationArtifacts"
+DATA_DIRECTORY = "data"
+DATA_FILE_NAME = "data.xlsx"
+TRAIN_ANNOTATION_FILE_NAME = "train_annotations.json"
+TEST_ANNOTATION_FILE_NAME = "test_annotations.json"
+SPACY_TRAIN_DATA_FORMAT_NAME = "train_data.spacy"
+SPACY_TEST_DATA_FORMAT_NAME = "test_data.spacy"
+
+MODEL_TRAINER_ARTIFACTS_DIR = "ModelTrainerArtifacts"
+CONFIG_FILE_NAME = "config.cfg"
+CONFIG_FILE_DIR = "config"
+
+BEST_MODEL_DIR = "model-best"
+OUTPUT_FILE_NAME = "output.xlsx"
+
+APP_HOST = "127.0.0.1"
+PORT = 8080
